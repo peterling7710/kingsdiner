7af5399da9026b44ce5287fb03043e88f1163077
Peter Ling <peterl732@hotmail.com>
2020-01-10 05:20:45 -0500

    react-pdf

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "typeface-barlow";
-import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
