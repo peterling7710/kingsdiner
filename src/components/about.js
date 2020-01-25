f1ed43d2b8b2f525cb6b558422084295baf180f9
Peter Ling <peterl732@hotmail.com>
2020-01-25 12:10:26 -0500

    hosted on beanstalk

--- src/components/about.js
+++ src/components/about.js
@@ -17,7 +17,7 @@ class About extends React.Component{
                     
                     <Card className="aboutCard">
                         <Card.Content>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                            Beanstalk. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
                             ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
                             magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
                             ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
